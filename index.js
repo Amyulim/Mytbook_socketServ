b9c3785be9c6729ed405d11cf5298ef85ae94de7
Eveyen <eveyenbcit@gmail.com>
2019-02-26 11:12:43 -0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const port = process.ev.PORT || 8800
 
-const server = app.listen(8800,(err)=>{
+app.all('/',function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With");
+  next();
+});
+const server = app.listen(port,(err)=>{
 	if (err){
 		console.log(err);
 		return false;
 	}
-	console.log( "8800 is opened");
+	console.log( port + " is opened");
 });
 
 var io = require("socket.io")(server);
